9350219433a8e1e7261a431150aea7eef0f72672
MakerSquare Student <student@instructor-3.att.net>
2013-07-15 14:56:56 -0500

    finished book model

--- db/migrate/20130715194933_create_books.rb
+++ db/migrate/20130715194933_create_books.rb
@@ -1,7 +1,10 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-    
+    	t.string :title
+    	t.string :author
+    	t.integer :year
+    	t.string :ISBN
       t.timestamps
     end
   end
